af15376efdda7a0af990b7172b653cb536127b20
Paulo Abreu <abreups@gmail.com>
2013-10-01 16:04:25 -0300

    index and scripts-main

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 var App = {
 	Models: {},
 	Collections: {},
-	Contacts: null
+	Views: {},
+	Contacts: null,
+	Directory: null
 };
 
 $(function() { // Run this code when the DOM is ready
@@ -17,27 +19,16 @@ $(function() { // Run this code when the DOM is ready
 	App.Contacts.add({
 		firstName: 'Rupert', lastName: 'Styx', phoneNumber: '9895551212', email: 'rupertstyx@example.com'
 	});
-	App.Contacts.add({});
 
-
-	var contactListing = '';
-
-	// Para cada um dos 3 contatos adicionados:
-	// 'contact' recebe o model da collection para cada iteração
-	App.Contacts.each(function(contact) {
-		contactListing += "<div>" + contact.get('firstName') + " " + contact.get('lastName') + " ";
-		if (contact.isValid()) { // roda a função Validate do model.
-			contactListing += "(valid)";
-		}
-		else {
-			contactListing += "(invalid)";
-		}
-		contactListing += "</div>";
+	App.Directory = new App.Views.Directory({
+		el: $('#display')
 	});
 
-	// display na div com id='display'
-	$('#display').html(contactListing);
+	App.Directory.render();
 
+	App.Contacts.on('add remove', function() {
+		App.Directory.render();
+	});
 });
 
 
